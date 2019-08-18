8c7fc231d96dcd391dd8fc354190af9db88533d9
Rigas <rigas@localhost.localdomain>
2019-08-18 23:05:59 +0100

    More code implemented

--- src/main/scala/com/kafkaRecords/KafkaRecords.scala
+++ src/main/scala/com/kafkaRecords/KafkaRecords.scala
@@ -1,14 +1,13 @@
 package com.kafkaRecords
 
 import java.time.OffsetDateTime
-
 import scala.concurrent.duration.Duration
 
 sealed trait record
 
-case class Start(userId: String,contentId: String,eventTimestamp: OffsetDateTime) extends record
+case class Start(userId: String,contentId: String) extends record
 
-case class Stop(userid: String,eventTimestamp: OffsetDateTime) extends record
+case class Stop(userId: String) extends record
 
 case class ContentWatchedEvents(userId: String, contentId: String, startTimestamp: OffsetDateTime, timeWatched: Duration) extends record
 
